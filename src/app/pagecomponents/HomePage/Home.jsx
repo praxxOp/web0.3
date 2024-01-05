9c47d54ab51b717aed33b2f0a4c76257623194b9
prathamesh <shelke.prathamesh0012@gmail.com>
2024-01-05 18:49:14 +0530

    modified:   next.config.js 	modified:   package-lock.json 	modified:   package.json 	new file:   public/1Kte.mp4 	deleted:    public/next.svg 	deleted:    public/vercel.svg 	new file:   src/app/assets/1Kte.webm 	modified:   src/app/pagecomponents/HomePage/Home.css 	modified:   src/app/pagecomponents/HomePage/Home.jsx

--- src/app/pagecomponents/HomePage/Home.jsx
+++ src/app/pagecomponents/HomePage/Home.jsx
@@ -1,25 +1,28 @@
 import Web3 from "@/app/reusablecomponents/Web3";
 import React from "react";
+
 import { michroma, syncopate } from "../../Fonts";
 import "./Home.css";
 
+
 const Home = () => {
   return (
-    <div>
-      <div className="Home__main">
-        <div className="Home__left">
-          <h3 className={`${syncopate.className} Home__headline`}>
-            <span>
-              {" "}
-              "Web3: Unlock NFTs, <br /> Secure <br />{" "}
-            </span>
-            Transactions”
-          </h3>
-          <Web3 />
-        </div>
-        <div className="Home__mid"></div>
-        <div className="HOme__right"></div>
+    <div className="Home__main">
+      <div className="Home__left">
+        <h3 className={`${syncopate.className} Home__headline`}>
+          <span>
+            "Web3: Unlock NFTs, <br /> Secure <br />
+          </span>
+          Transactions”
+        </h3>
+        <Web3 />
       </div>
+      <div className="Home__mid">
+        <video autoPlay loop muted>
+          <source src="/1Kte.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div className="Home__right"></div>
     </div>
   );
 };
