6ee1a2a6c0685fe04da3cee14046707182117e1b
prathamesh <shelke.prathamesh0012@gmail.com>
2024-01-11 12:26:52 +0530

    modified:   src/app/layoutcomponents/navbar/NavBar.jsx 	modified:   src/app/layoutcomponents/navbar/Navbar.css 	modified:   src/app/pagecomponents/HomePage/Home.css

--- src/app/layoutcomponents/navbar/NavBar.jsx
+++ src/app/layoutcomponents/navbar/NavBar.jsx
@@ -1,14 +1,49 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import "./Navbar.css";
 import { michroma, nico } from "../../Fonts";
 
 const NavBar = () => {
+  const [Toggle, setToggle] = useState(false);
+  const FirstToggle = () => {
+    setToggle(!Toggle);
+  };
+  const [BuyToggle, setBuyToggle] = useState(false);
+
+  const SecondToggle = () => {
+    setBuyToggle(!BuyToggle);
+  };
+
   return (
     <div className="navbar">
       <div className="Navbar__content">
-        <h4 className={` ${michroma.className} Navbar__h1 `}>GET ACCESS</h4>
+        <h4
+          onClick={FirstToggle}
+          className={` ${michroma.className} Navbar__h1 `}
+        >
+          GET ACCESS
+          {Toggle ? (
+            <div className="access__options">
+              <h4>Sign-IN</h4>
+              <h4>Register</h4>
+            </div>
+          ) : null}
+        </h4>
         <h1 className={`${nico.className} Navbar__logo`}>WEB3</h1>
-        <h4 className={` ${michroma.className} Navbar__h1 `}>BUY & SELL</h4>
+        <h4
+          onClick={SecondToggle}
+          className={` ${michroma.className} Navbar__h1 `}
+        >
+          BUY & SELL
+          {BuyToggle ? (
+            <div className="buy__options">
+              <h4>Marketplace </h4>
+              <h4>Catalogue</h4>
+              <h4>Community</h4>
+            </div>
+          ) : null}
+        </h4>
       </div>
     </div>
   );
