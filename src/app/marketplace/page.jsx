5d68453538490ea5826112c026574d89df3ce2b3
prathamesh <shelke.prathamesh0012@gmail.com>
2024-01-15 15:59:09 +0530

    renamed:    "public/\360\237\232\247Under Construction\360\237\232\247.mp4" -> public/background.mp4 	modified:   src/app/marketplace/Marketplace.css 	modified:   src/app/marketplace/page.jsx

--- src/app/marketplace/page.jsx
+++ src/app/marketplace/page.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import "./Marketplace.css";
 import { delaGothicOne, michroma, sora } from "../Fonts";
+
 const MarketPlace = () => {
   return (
     <div className="main__market">
       <div className="market__video">
+        <video autoPlay muted loop>
+          <source src="/background.mp4" type="video/mp4" />
+        </video>
+      </div>
+      <div className="content__marketplace">
         <h1 className={michroma.className}>UNDER</h1>
         <h1 className={delaGothicOne.className}>CONSTRUCTION</h1>
-        <p className={ sora.className}>
+        <p className={sora.className}>
           Get ready for a game-changing NFT marketplace! We're currently under
           construction, crafting an innovative space where creativity meets
           ownership. Stay tuned for a groundbreaking experience in digital art
