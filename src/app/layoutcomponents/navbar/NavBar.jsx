7d0361159cbbcaa60630d9fc4a346b51fc1a72e1
prathamesh <shelke.prathamesh0012@gmail.com>
2024-01-22 16:58:29 +0530

    modified:   src/app/layoutcomponents/navbar/NavBar.jsx 	modified:   src/app/layoutcomponents/navbar/Navbar.css

--- src/app/layoutcomponents/navbar/NavBar.jsx
+++ src/app/layoutcomponents/navbar/NavBar.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import "./Navbar.css";
 import { michroma, nico } from "../../Fonts";
-
+import { AnimatePresence, motion } from "framer-motion";
 
 const NavBar = () => {
   const [Toggle, setToggle] = useState(false);
@@ -16,6 +16,21 @@ const NavBar = () => {
     setBuyToggle(!BuyToggle);
   };
 
+  const menu = {
+    visible: {
+      y: "0",
+      opacity: 1,
+      transition: {
+        type: "spring",
+      },
+    },
+
+    hidden: {
+      y: "-10px",
+      opacity: 0,
+    },
+  };
+
   return (
     <div className="navbar">
       <div className="Navbar__content">
@@ -24,12 +39,25 @@ const NavBar = () => {
           className={` ${michroma.className} Navbar__h1 `}
         >
           GET ACCESS
-          {Toggle ? (
-            <div className="access__options">
-              <h4>Sign-IN</h4>
-              <h4>Register</h4>
-            </div>
-          ) : null}
+          <AnimatePresence>
+            {Toggle ? (
+              <motion.div
+                variants={menu}
+                initial="hidden"
+                animate="visible"
+                exit={{
+                  y: "10px",
+                  opacity: 0,
+                }}
+                className="access__options"
+              >
+                <h4>
+                  Sign-IN With <br />
+                  Google
+                </h4>
+              </motion.div>
+            ) : null}
+          </AnimatePresence>
         </h4>
         <h1 className={`${nico.className} Navbar__logo href="#home"`}>WEB3</h1>
         <h4
@@ -37,13 +65,24 @@ const NavBar = () => {
           className={` ${michroma.className} Navbar__h1 `}
         >
           BUY & SELL
-          {BuyToggle ? (
-            <div className="buy__options">
-              <h4>Catalogue</h4>
-              <h4>Marketplace </h4>
-              <h4>Community</h4>
-            </div>
-          ) : null}
+          <AnimatePresence>
+            {BuyToggle ? (
+              <motion.div
+                variants={menu}
+                initial="hidden"
+                animate="visible"
+                exit={{
+                  y: "10px",
+                  opacity: 0,
+                }}
+                className="buy__options"
+              >
+                <h4>Catalogue</h4>
+                <h4>Marketplace </h4>
+                <h4>Community</h4>
+              </motion.div>
+            ) : null}
+          </AnimatePresence>
         </h4>
       </div>
     </div>
