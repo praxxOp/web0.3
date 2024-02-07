ab7a3018715b6bacc523db0327a2510c44643e53
prathamesh <shelke.prathamesh0012@gmail.com>
2024-02-07 15:34:14 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/app/layout.js 	modified:   src/app/page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import Home from './pagecomponents/HomePage/Home'
-import Nft from './pagecomponents/NftPage/Nft'
-import Portfolio from './pagecomponents/Portfolio/Portfolio'
-import Community from './pagecomponents/CommunityPage/Community'
+import React from "react";
+import Home from "./pagecomponents/HomePage/Home";
+import Nft from "./pagecomponents/NftPage/Nft";
+import Portfolio from "./pagecomponents/Portfolio/Portfolio";
+import Community from "./pagecomponents/CommunityPage/Community";
 
 const page = () => {
   return (
-    <div>    
-      <Home/>
-      <Nft/>
-      <Portfolio/>
-      <Community/>
+    <div>
+      <Home />
+      <Nft />
+      <Portfolio />
+      <Community />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
